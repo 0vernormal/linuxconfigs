38101a653f763e9d090590668f17e8a1374071cb
0vernormal <topraq@tuta.io>
2024-12-19 19:47:57 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+TARGET_DIR=~/gitthings/linuxconfigs
+TARGET_SCRIPT="$TARGET_DIR/setup.sh"
+CONFIG_DIRS=("alacritty" "conky" "picom" "qtile" "rofi")
+CONFIG_BASE=~/.config
+
+CURRENT_SCRIPT="$(realpath "$0")"
+CURRENT_DIR="$(dirname "$CURRENT_SCRIPT")"
+
+if [[ "$CURRENT_SCRIPT" != "$TARGET_SCRIPT" ]]; then
+    mkdir -p "$TARGET_DIR"
+    cp -r "$CURRENT_DIR/"* "$TARGET_DIR/"
+    chmod +x "$TARGET_SCRIPT"
+    echo "Betik doğru konuma taşındı: $TARGET_SCRIPT"
+    exec "$TARGET_SCRIPT" "$@"
+fi
+
+for dir in "${CONFIG_DIRS[@]}"; do
+    SRC="$TARGET_DIR/$dir"
+    DEST="$CONFIG_BASE/$dir"
+
+    if [[ -d "$DEST" || -f "$DEST" ]]; then
+        mv "$DEST" "$DEST.old"
+    fi
+
+    mkdir -p "$CONFIG_BASE"
+    cp -r "$SRC" "$DEST"
+done
+
+if [[ -f "$TARGET_DIR/mypackages.txt" ]]; then
+    yay -S --noconfirm $(cat ~/gitthings/linuxconfigs/mypackages.txt)
+fi
